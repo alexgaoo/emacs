8cf795483825aa590aed3c71903bf65cb8c9472c
Alex Gao <alexgao12@gmail.com>
2016-08-17 15:46:36 -0700

    Create init.el

---
+++ init.el
@@ -0,0 +1,19 @@
+(load-theme 'material t)
+
+(menu-bar-mode -1)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
+(require 'magit)
+
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+	       "~/.emacs.d/site-lisp/magit/Documentation/"))
